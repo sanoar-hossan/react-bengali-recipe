b27b3634e69cb828614169f34e4008ef3ada6bb0
chayan <admin@ditskill.com>
2023-05-02 16:12:05 +0600

    environment setup

---
+++ react-authentication/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDx48DPYuG9_X3qxQBuFN8zhU8NEEE_ELU",
+  authDomain: "react-authentication-ba55f.firebaseapp.com",
+  projectId: "react-authentication-ba55f",
+  storageBucket: "react-authentication-ba55f.appspot.com",
+  messagingSenderId: "681272698926",
+  appId: "1:681272698926:web:b2b233d8728801559857bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
